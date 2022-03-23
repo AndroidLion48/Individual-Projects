2c744173f9bc90686fd2753e47844fc2edf79645
AndroidLion48 <>
2022-03-22 21:58:34 -0700

    Created Game screen for Zombie Bird and attached it

--- Zombie_Bird/core/src/com/zombie_bird/game/MyGame.java
+++ Zombie_Bird/core/src/com/zombie_bird/game/MyGame.java
@@ -1,31 +1,45 @@
 package com.zombie_bird.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.zombie_bird.game.screens.GameScreen;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class MyGame extends Game {
+
+
+  @Override
+  public void create() {
+    Gdx.app.log("ZBGame", "created");
+    setScreen(new GameScreen());
+  }
 }
+
+
+
+
+
+
+//	SpriteBatch batch;
+//	Texture img;
+//
+//	@Override
+//	public void create () {
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+//	}
+//
+//	@Override
+//	public void render () {
+//		ScreenUtils.clear(1, 0, 0, 1);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
+
